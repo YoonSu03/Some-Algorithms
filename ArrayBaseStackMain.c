8b791655e2f3794c2e26fd21749cdad94e8b7481
PlanNoa <37352488+Leeheewoong@users.noreply.github.com>
2018-09-18 15:13:18 +0900

    stack
    stack

---
+++ ArrayBaseStackMain.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "ArrayBaseStack.h"
+
+int main(void) 
+{
+	
+	Stack stack;
+	StackInit(&stack);
+
+	SPush(&stack, 1);
+	SPush(&stack, 2);
+	SPush(&stack, 3);
+	SPush(&stack, 4);
+	SPush(&stack, 5);
+
+	while (!SIsEmpty(&stack))
+		printf("%d", SPop(&stack));
+
+	return 0;
+}
